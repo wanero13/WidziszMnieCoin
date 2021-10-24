c3e4da6534669161244d0dbc02a78c3f72c16e04
wanero13 <01130999@pw.edu.pl>
2021-10-24 22:50:00 +0200

    Etap 1 Wersja 1

--- blockchainManager.py
+++ blockchainManager.py
@@ -1,4 +1,47 @@
+import json
+import hashlib
+
 class blockchainManager:
 
     def __init__(self):
-        self.sumHash = 0
+        self.chain = []
+
+        genessisBlock = {
+            'id': len(self.chain)+1,
+            'prevHash': 0,
+            'content': 'content',
+        }
+        self.chain.append(genessisBlock)
+        toCode = json.dumps(genessisBlock, sort_keys=1).encode()
+        self.sumHash = hashlib.sha3_512(toCode).hexdigest()
+
+    def checkValid(self):
+        prevHash = 0
+        flag = 0
+        for block in self.chain:
+            if block['prevHash'] != prevHash:
+                print('Wykryto brak spójności w bloku: ' + block)
+                flag = 1
+                break
+            prevBytes = json.dumps(block, sort_keys=1).encode()
+            prevHash = hashlib.sha3_512(prevBytes).hexdigest()
+        lastBlock = self.chain[-1]
+        lastBlockBytes = json.dumps(lastBlock, sort_keys=1).encode()
+        lastBlockHash = hashlib.sha3_512(lastBlockBytes).hexdigest()
+        if flag == 0:
+            if lastBlockHash != self.sumHash:
+                print('Wykryto brak spójności w bloku: ' + lastBlock)
+            else:
+                print('Spójność zachowana!')
+
+
+    def addBlock(self, content):
+        block = {
+            'id': len(self.chain)+1,
+            'prevHash': self.sumHash,
+            'content': content,
+        }
+        self.chain.append(block)
+        toCode = json.dumps(block,sort_keys=1).encode()
+        self.sumHash = hashlib.sha3_512(toCode).hexdigest()
+        print(self.chain)
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Alamakota")
+import blockchainManager
+
+
+Blockchain = blockchainManager.blockchainManager()
+
+while True:
+    print("1 - Dodaj blok")
+    print("2 - Sprawdź spójność")
+    print("3 - Zakończ")
+    option = input()
+    if option == '1':
+        print("Podaj zawartość")
+        content = input()
+        Blockchain.addBlock(content)
+    elif option == '2':
+        Blockchain.checkValid()
+    elif option == '3':
+        exit(1)
+    else:
+        print("Wybierz 1 lub 2")
