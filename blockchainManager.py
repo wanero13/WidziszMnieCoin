f5375ff0398945348e06de37e3e44281911fb78f
Adrian Gabor <adgabor97@gmail.com>
2021-11-22 15:14:42 +0100

    Etap 2 Wersja 1

--- blockchainManager.py
+++ blockchainManager.py
@@ -1,10 +1,35 @@
 import json
 import hashlib
+from time import *
+
+import binascii
+from matplotlib import pylab
+import pylab as pl
+
+import Crypto
+import Crypto.Random
+from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+
+
+class Client:
+   def __init__(self):
+      random = Crypto.Random.new().read
+      self._private_key = RSA.generate(1024, random)
+      self._public_key = self._private_key.publickey()
+      self._signer = PKCS1_v1_5.new(self._private_key)
+
+   @property
+   def identity(self):
+    return binascii.hexlify(self._public_key.exportKey(format='DER')).decode('ascii')
+
 
 class blockchainManager:
 
     def __init__(self):
         self.chain = []
+        self.pending_transactions = []
 
         genessisBlock = {
             'id': len(self.chain)+1,
@@ -35,13 +60,30 @@ class blockchainManager:
                 print('Spójność zachowana!')
 
 
-    def addBlock(self, content):
+    def addBlock(self, proof):
         block = {
             'id': len(self.chain)+1,
             'prevHash': self.sumHash,
-            'content': content,
+            'timetamp': strftime("%d %b %Y %H:%M:%S", gmtime()),
+            'transactions': self.pending_transactions,
+            'proof' : proof,
         }
+        self.pending_transactions = []
         self.chain.append(block)
         toCode = json.dumps(block,sort_keys=1).encode()
         self.sumHash = hashlib.sha3_512(toCode).hexdigest()
         print(self.chain)
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    def new_transaction(self, sender, recipient, amount):
+        transaction = {
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        }
+        self.pending_transactions.append(transaction)
+        return self.last_block['id'] + 1
+
--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import blockchainManager
 
-
 Blockchain = blockchainManager.blockchainManager()
 
-while True:
-    print("1 - Dodaj blok")
-    print("2 - Sprawdź spójność")
-    print("3 - Zakończ")
-    option = input()
-    if option == '1':
-        print("Podaj zawartość")
-        content = input()
-        Blockchain.addBlock(content)
-    elif option == '2':
-        Blockchain.checkValid()
-    elif option == '3':
-        exit(1)
-    else:
-        print("Wybierz 1 lub 2")
+# while True:
+#     print("1 - Dodaj blok")
+#     print("2 - Sprawdź spójność")
+#     print("3 - Zakończ")
+#     option = input()
+#     if option == '1':
+#         print("Podaj zawartość")
+#         content = input()
+#         Blockchain.addBlock(content)
+#     elif option == '2':
+#         Blockchain.checkValid()
+#     elif option == '3':
+#         exit(1)
+#     else:
+#         print("Wybierz 1 lub 2")
+
+User1 = blockchainManager.Client()
+User2 = blockchainManager.Client()
+
+t1 = Blockchain.new_transaction(User1.identity, User2.identity, '5btc')
+t2 = Blockchain.new_transaction(User1.identity, User2.identity, '5btc')
+Blockchain.addBlock(1)
+
+t3 = Blockchain.new_transaction("user3", "user4", '2btc')
+t4 = Blockchain.new_transaction("user2", "user4", '1btc')
+Blockchain.addBlock(2)
+print("Blockchain: ", Blockchain.chain)
